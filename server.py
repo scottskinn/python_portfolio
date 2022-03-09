81a0ea75e9db1db1e70b8f41ab86297d703b5035
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-03-09 14:00:08 -0500

    submit form csv

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 print(__name__)
@@ -17,14 +18,26 @@ def write_to_file(data):
         subject = data["subject"]
         message = data["message"]
         file = database.write(f'\n{email},{subject},{message}')
+        
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=",", quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+        # headers are not showing ???
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
-        print(data)
-        return redirect('/thankyou.html')
+        try:
+            data = request.form.to_dict()
+            write_to_file(data)
+            print(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
     else:
         return 'something went wrong'
 
